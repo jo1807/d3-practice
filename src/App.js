958b98086ec6938a5a4092c6603c5f508efd6c82
Joanne Notley <joannenotley@joannes-mbp-2.lan>
2019-12-16 11:36:39 +0000

    add basic bar chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import * as d3 from "d3";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    drawChart();
+  }, []);
+
+  const drawChart = () => {
+    //const data = [12, 5, 6, 6, 9, 10];
+    const data = [1, 2, 3, 4, 5, 6];
+    const w = 500;
+    const h = 400;
+    const svg = d3
+      .select("body")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .style("margin-left", 100);
+    svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => i * 70)
+      .attr("y", (d, i) => h - 10 * d)
+      .attr("width", 65)
+      .attr("height", (d, i) => d * 10)
+      .attr("fill", "green")
+      .on("mouseover", (d, i) =>
+        d3
+          .select(this)
+          .transition()
+          .duration("200")
+          .attr("rect", 5)
+      );
+  };
+
+  return <div id={"bar-chart"}></div>;
 }
 
 export default App;
